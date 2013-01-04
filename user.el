366c4c5e3f875dc27af607e08c0ef0b2b93a9ced
Konstantin Rudy <konstantin.rudy@gmail.com>
2013-01-04 21:35:14 +0300

    Added file_rename function

--- user.el
+++ user.el
@@ -21,6 +21,14 @@
 ;; Show line numbers
 (global-linum-mode 1)
 
+;; Beginning of the el4r block:
+;; RCtool generated this block automatically. DO NOT MODIFY this block!
+(add-to-list 'load-path "~/.rvm/rubies/ruby-1.9.3-p327/share/emacs/site-lisp")
+(require 'el4r)
+;;(el4r-boot)
+;; End of the el4r block.
+;; User-setting area is below this line.
+
 ;; Robe initialization
 (add-hook 'ruby-mode-hook 'robe-mode)
 
