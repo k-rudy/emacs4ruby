d880768a009d1c38efb3679f3b421d91b501c0ee
Konstantin Rudy <konstantin.rudy@gmail.com>
2013-01-05 09:37:32 +0300

    Commented out el4r section

--- user.el
+++ user.el
@@ -23,8 +23,8 @@
 
 ;; Beginning of the el4r block:
 ;; RCtool generated this block automatically. DO NOT MODIFY this block!
-(add-to-list 'load-path "~/.rvm/rubies/ruby-1.9.3-p327/share/emacs/site-lisp")
-(require 'el4r)
+;;(add-to-list 'load-path "~/.rvm/rubies/ruby-1.9.3-p327/share/emacs/site-lisp")
+;;(require 'el4r)
 ;;(el4r-boot)
 ;; End of the el4r block.
 ;; User-setting area is below this line.
